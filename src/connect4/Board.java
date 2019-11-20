dad13fd68319c059dfb16c5deb4777a830ec62ef
alextenczar <alextenczar@gmail.com>
2019-11-19 22:40:47 -0500

    Initial Commit

---
+++ src/connect4/Board.java
@@ -0,0 +1,59 @@
+package connect4;
+
+public class Board {
+	
+	char playerOne;
+	char playerTwo;
+	
+	char[][] boardArray;
+	
+	public Board(char playerOne, char playerTwo){
+		boardArray = new char[6][7];
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+	}
+	
+	public char getPlayerOne()
+	{
+		return playerOne;
+	}
+	
+	public char getPlayerTwo()
+	{
+		return playerTwo;
+	}
+	
+	
+	//clears board and fills it with emptyPiece variable
+	public char[][] clearBoard(Board b){
+		char emptyP = 'O';
+		boardArray = b.getBoard(b);
+		return boardArray;
+	}
+	
+	//executes turn by prompting player to place piece
+	public void turn(char player, int column ){
+		
+	}
+	
+	//checks whether p1 or p2 has won the game
+	public boolean checkWin(char[][]board, char playerOne, char playerTwo) {
+		//if row is found return true
+		if 
+		{ 
+			return true;
+		}
+		else //if no rows found return false
+		{
+			return false;
+		}
+	}
+	
+	//used for printing our board array
+	public char[][] getBoard(Board b)
+	{
+		
+		return boardarray;
+	}
+	
+}
