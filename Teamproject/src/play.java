8449decc40e46404b13fbf9fc23e21f55e760021
jacob-blomq <35968186+jacob-blomq@users.noreply.github.com>
2019-12-15 12:44:03 -0500

    Update play.java

--- Teamproject/src/play.java
+++ Teamproject/src/play.java
@@ -2,109 +2,25 @@ import java.util.Scanner;
 
 public class play {
 	public static void main(String[] args)
-	{	
+	{
 		while(true)
 		{
-			//Variable Initialization
-			int col;
-			int cp;
-			char pT;
-			char pO;
-			boolean valid;
-			
-			//Player Initialization
+			int choice = 0;
 			Scanner scan = new Scanner(System.in);
-			System.out.println("Player 1 Select a Piece (any Character)");
-			pO = scan.next().charAt(0);
-			System.out.println("Player 2 Select a Piece (any Character)");
-			pT = scan.next().charAt(0);
+			System.out.println("Please pick a mode (1 = 1 player 2 = 2 player):");
+			choice = scan.nextInt();
 			
-			//Board Initialization
-			Board board = new Board(pO, pT);
-			System.out.println("-------------Setting up Board-------------");
-			board.clearBoard(board);
-			
-			//Deciding First Player
-			System.out.println("--------Let's chose who goes first---------");
-			board.decideturn();
-			cp = board.currentplayer();
-			System.out.println("\nPlayer " + (cp+1) + " your turn first!");	
-			board.printBoard(board.getBoard(board));
-			
-			//Game Loop
-			while (true)
-			{ 
-				System.out.print("Player " + (cp+1) + " Pick a Column: ");
-				col = scan.nextInt();
-				if(col > 7)
-				{
-					System.out.println("Please Pick a Column 1-7!");
-					col = scan.nextInt();
-				}
-				else
-				{
-					valid = board.turn(cp, col, board);
-					board.printBoard(board.getBoard(board));
-					if (board.turnValid == true){
-						board.turnCount++;
-					}
-					System.out.println(board.turnCount);
-					// Here we will need to board.checkWin()
-					// if checkWin is true, we need to know who won and add 1 or 2 to board.winner.
-					if (board.winner == 1) {
-						board.endGame();
-						break;
-					}
-					if (board.winner == 2) {
-						board.endGame();
-						break;
-					}
-					if (board.turnCount == 42){
-						board.endGame();
-						break;
-					}
-					while(valid == false)
-					{
-						System.out.print("Player " + (cp+1) + " Pick a Column: ");
-						col = scan.nextInt();
-						if(col > 7)
-						{
-							System.out.println("Please Pick a Column 1-7!");
-							col = scan.nextInt();
-						}
-						else
-						{
-							valid = board.turn(cp, col, board);
-							board.printBoard(board.getBoard(board));
-							if (board.turnValid == true){
-								board.turnCount++;
-							}
-							System.out.println(board.turnCount);
-							// Here we will need to board.checkWin()
-							if (board.winner == 1) {
-								board.endGame();
-								break;
-							}
-							if (board.winner == 2) {
-								board.endGame();
-								break;
-							}
-							if (board.turnCount == 42){
-								board.endGame();
-								break;
-							}
-						}
-							
-					}
-					cp = (cp + 1) % 2;
-				}
-			}
-			System.out.print("Play Again?: Y/N: ");
-			String decision = scan.next();
-			if(decision.equals("n") || decision.equals("N"))
+			if(choice == 1)
 			{
-				break;
+				OnePlayer Oneplayer = new OnePlayer();
+				Oneplayer.play();
 			}
+			if(choice == 2)
+			{
+				TwoPlayer Twoplayer = new TwoPlayer();
+				Twoplayer.play();
+			}
+			
 		}
 	}		
 }
